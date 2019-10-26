99867ce0083294a88f1b353ed80a0a1e9e1823af
lukeliu <1liuluk@hdsb.ca>
2019-10-26 08:27:47 -0700

    Initial Commit

---
+++ NutriFitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NutriFitness
+//
+//  Created by Luke Liu on 2019-10-26.
+//  Copyright © 2019 Luke Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
